33ba156e3bf729edd9c5c6ef4a20e14c5c2d9a92
Ain Ruiz Dorado <ain.ruiz.dorado@gmail.com>
2023-07-05 17:17:17 -0300

    nuevos cambios

--- lua/lsp/telescope.lua
+++ lua/lsp/telescope.lua
@@ -2,16 +2,25 @@ local telescope = require('telescope')
 local actions = require('telescope.actions')
 
 telescope.setup {
-    defaults = {
-        color_devicons = true,
-        prompt_prefix = "$ ",
-        file_ignore_patterns = { "^.git/", "^.node_modules/" },
-        mappings = {
-            i = {
-                ['<esc>'] = actions.close
-            }
-        }
+  defaults = {
+    -- layout_strategy = "vertical",
+    -- layout_config = {
+    --   vertical = {
+    --     width = 0.9,
+    --     height = 0.9,
+    --     preview_height = 0.6,
+    --     preview_cutoff = 0
+    --   }
+    -- },
+    color_devicons = true,
+    prompt_prefix = "$ ",
+    file_ignore_patterns = { "^.git/", "^.node_modules/" },
+    mappings = {
+      i = {
+        ['<esc>'] = actions.close
+      }
     }
+  }
 }
 
 -- To get fzf loaded and working with telescope, you need to call
@@ -21,17 +30,17 @@ telescope.setup {
 local M = {}
 
 M.search_dotfiles = function()
-    require('telescope.builtin').find_files({
-        prompt_title = "<.dotfiles>",
-        cwd = '~/.dotfiles',
-        hidden = true
-    })
+  require('telescope.builtin').find_files({
+    prompt_title = "<.dotfiles>",
+    cwd = '~/.dotfiles',
+    hidden = true
+  })
 end
 
 M.project_files = function()
-    local opts = {}
-    local ok = pcall(require('telescope.builtin').git_files, opts)
-    if not ok then require('telescope.builtin').find_files(opts) end
+  local opts = {}
+  local ok = pcall(require('telescope.builtin').git_files, opts)
+  if not ok then require('telescope.builtin').find_files(opts) end
 end
 
 return M
